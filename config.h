a997eda2fdcaa281763914e55defb737044a51fe
Wilmer Nilsson <wlmr@pm.me>
2018-11-27 16:35:14 +0100

    bhextra added as a static var in config header

--- config.h
+++ config.h
@@ -4,17 +4,19 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-          "artwiz cure-8"
+          "dina-6:regular"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#ffffff", "#101010" },
-	[SchemeSel] = { "#eeeeee", "#2835f7" },
+	[SchemeSel] = { "#eeeeee", "#285577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* the number of pixles to increase vertical padding with */
+static unsigned int bhextra    = 6;
 
 /*
  * Characters not considered part of a word while deleting words
